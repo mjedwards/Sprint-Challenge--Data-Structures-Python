c5a50cc3158408ede57433c1c159a03b627817a7
Michael Edwards <ruoverlord@Michaels-MacBook-Pro.local>
2020-08-28 20:59:12 -0400

    initial commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.amg = []
+        # self.cur = -1
 
     def append(self, item):
-        pass
+        # if len(self.amg) == self.capacity:
+        #     return self.amg
+
+        # else:
+        #     self.cur = (self.cur + 1) % self.capacity
+        #     self.amg[self.cur] = self.amg.append(item)
+        if len(self.amg) == self.capacity:
+            return self.amg
+
+        else:
+            self.amg.append(item)
+            
+            
 
     def get(self):
-        pass
+        return self.amg
+
+        
